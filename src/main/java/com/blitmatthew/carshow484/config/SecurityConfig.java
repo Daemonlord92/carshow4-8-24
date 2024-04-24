549312856416ff0bd7da6f675e8d9e0ff2ac3425
Matthew Jeshua Martin <matthew-martin@programmer.net>
2024-04-24 15:44:20 -0400

    cors added

--- src/main/java/com/blitmatthew/carshow484/config/SecurityConfig.java
+++ src/main/java/com/blitmatthew/carshow484/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT, "/api/v1/car/").hasAnyRole("ADMIN")
                         .anyRequest().authenticated());*/
         http
-                .cors(c -> c.disable())
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .csrf(c -> c.disable())
                 .authorizeHttpRequests( c -> c
                         .requestMatchers("/api/v1/auth/**", "/api/v1/car/").permitAll()
@@ -45,5 +47,16 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
 }
